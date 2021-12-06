82633bfbf7e04071bf51b45f6998be973d79b4d9
ahmadrockets <ahmadrockets@gmail.com>
2021-12-06 11:51:48 +0700

    Update setting connect to mysql

--- webGis/core/settings.py
+++ webGis/core/settings.py
@@ -78,11 +78,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'django_login',
+        'USER': 'sammy',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',   
+        'PORT': '3306',
+    }   
 }
 
 
